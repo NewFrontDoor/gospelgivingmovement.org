821a8b013018cd36c7f2212741f366d52752b9c5
readeral <alan@readeral.com>
2020-10-14 11:19:57 +1100

    adding event details

--- src/components/pages/frontpage/Events.js
+++ src/components/pages/frontpage/Events.js
@@ -8,9 +8,17 @@ export default () => (
                     <h2 className="headerPink">BE OUR GUEST!</h2>
                     <hr className="hrPink" />
                     <p>
-                        Each year we host casual and intimate events in Tasmania to mingle between ministry staff and gospel patrons, to share ideas, advice and encouragement.
+                        Each year we host casual and intimate events in Tasmania to mingle between ministry staff and gospel patrons, to share ideas, advice and encouragement. This year we are holding an online forum in the last week of November. There are two possible dates for this event, please indicate in the form below.
                     </p>
-
+                    <ul>
+                        <li>
+                            Starting at 7:00pm
+                        </li>
+                        <li>Finishing at 7:50pm</li>
+                        <li>Brief Keynote from Mikey Lynch and another Gospel Giving patron</li>
+                        <li>A chance to share ideas, encourage one another, provide valuable input from a supporter's perspective into Gospel ministry.</li>
+                    </ul>
+                    <iframe title="Gospel Giving Forum Registration" allowtransparency="true" src="https://ufcutas.elvanto.com.au/form/9493d547-d340-457a-8f1e-8efa4380d919" frameborder="0" style={{width: "100%", height: "720px", border: "none"}}></iframe>
                 </div>
             </div>
         </div>
